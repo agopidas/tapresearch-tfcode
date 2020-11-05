b0e48abf159bb437d46fe7e34ebbeefa390d619a
ag <agopidas@gmail.com>
2020-11-05 02:44:06 -0500

    Updated memcache tf code but only for testing purpose

---
+++ cache/test-elastic-memcahce-redis.tf
@@ -0,0 +1,18 @@
+resource "aws_elasticache_cluster" "example-memcache" {
+  cluster_id           = "cluster-example"
+  engine               = "memcached"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 2
+  parameter_group_name = "default.memcached1.4"
+  port                 = 11211
+}
+
+resource "aws_elasticache_cluster" "example-redis" {
+  cluster_id           = "cluster-example"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+}
